a1214ecf5fb19aef4907b612b7bcd0bc6d5405fe
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2023-11-12 15:26:19 +0200

    initialize repository

---
+++ EmailListType.ts
@@ -0,0 +1,26 @@
+import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
+
+const mailerSend = new MailerSend({
+  apiKey: process.env.API_KEY,
+});
+
+export const WelcomeEmail=()=>{
+const sentFrom = new Sender("you@yourdomain.com", "Your name");
+
+const recipients = [
+  new Recipient("your@client.com", "Your Client")
+];
+
+const emailParams = new EmailParams()
+  .setFrom(sentFrom)
+  .setTo(recipients)
+  .setReplyTo(sentFrom)
+  .setSubject("This is a Subject")
+  .setTemplateId('templateId');
+
+mailerSend.email.send(emailParams).then(res=>{
+
+}).catch(err=>{
+    
+})
+}
